9f1903b9ff802394f9776dcae5be78bd5ab28481
Guillermo Guerrero <g.guerrero.bus@gmail.com>
2016-02-04 11:42:19 +0100

    Fix ENV var. Add new services and modules to load.

--- isbndb/__init__.py
+++ isbndb/__init__.py
@@ -1,8 +1,17 @@
 import os
 
 # Loaded modules when * is called
-__all__  = ["parsedresponse", "book", "author"]
+__all__  = [
+  "baseservice",
+  "parsedresponse",
+  "services.authors",
+  "services.books",
+  "services.categories",
+  "services.prices",
+  "services.publishers",
+  "services.subjests"
+]
 
 # Constants, maybe used with ENV vars
 HOSTNAME = os.getenv('ISBNDB_HOSTNAME', 'isbndb.com')
-APIKEY   = os.getenv('ISBNDB_HOSTNAME', '45ATS15S')
+APIKEY   = os.getenv('ISBNDB_APIKEY',   '45ATS15S')
